a59e50e7f795edb578a6b71419c354b16e9fc15e
VDemyanov <vlad.demyanov.official@mail.ru>
2022-12-21 08:15:46 +0300

    Update lab 6

--- Labs/Lab6/Lab6.ConsoleClient/Program.cs
+++ Labs/Lab6/Lab6.ConsoleClient/Program.cs
@@ -10,11 +10,13 @@ namespace Lab6.ConsoleClient
         {
             var uri = new Uri("http://localhost:53215/Services/WsDvr.svc");
             var wsDvr = new WsDvrEntities(uri);
+            wsDvr.AddToStudents(new Students { Name = "Test"});
+            wsDvr.SaveChanges();
 
-            foreach (var student in wsDvr.Students)
-            {
-                Console.WriteLine(student.Id + " " + student.Name);
-            }
+            //foreach (var student in wsDvr.Students)
+            //{
+            //    Console.WriteLine(student.Id + " " + student.Name);
+            //}
         }
     }
 }
--- Labs/Lab6/Lab6.WebServices/Services/WsDvr.svc.cs
+++ Labs/Lab6/Lab6.WebServices/Services/WsDvr.svc.cs
@@ -21,7 +21,7 @@ namespace Lab6.WebServices.Services
         {
             // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
             // Examples:
-            config.SetEntitySetAccessRule("*", EntitySetRights.AllRead);
+            config.SetEntitySetAccessRule("*", EntitySetRights.AllRead | EntitySetRights.AllWrite);
             config.SetServiceOperationAccessRule("*", ServiceOperationRights.All);
             config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
         }
